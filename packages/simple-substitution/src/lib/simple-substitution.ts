dd8d5a3278060a6d98891be3395d2c36f58e99a0
Adarsh Dubey <dubeyadarshmain@gmail.com>
2023-10-30 19:59:11 +0530

    fix: :bug: fix implementation of simple subsitution cipher
    add implementation of captial letters in encode function. rewrite whole decode function as it was not producing correct output

--- packages/simple-substitution/src/lib/simple-substitution.ts
+++ packages/simple-substitution/src/lib/simple-substitution.ts
@@ -3,19 +3,23 @@ export function encode(input: string, key: string) {
     throw new Error('The key must contain 26 unique characters.');
   }
 
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const lowercaseKey = key.toLowerCase();
+  const uppercaseKey = key.toUpperCase();
+  const newKey = lowercaseKey + uppercaseKey;
+
+  const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const substitutionMap: Record<string, string> = {};
 
-  for (let i = 0; i < 26; i++) {
-    substitutionMap[alphabet[i]] = key[i];
+  for (let i = 0; i < 52; i++) {
+    substitutionMap[alphabet[i]] = newKey[i];
   }
 
-  const substitutedString = input
+  const encodedString = input
     .split('')
     .map((char) => (substitutionMap[char] ? substitutionMap[char] : char))
     .join('');
 
-  return substitutedString;
+  return encodedString;
 }
 
 export function decode(input: string, key: string) {
@@ -23,6 +27,21 @@ export function decode(input: string, key: string) {
     throw new Error('The key must contain 26 unique characters.');
   }
 
-  const reverseKey = key.split('').reverse().join('');
-  return encode(input, reverseKey);
+  const lowercaseKey = key.toLowerCase();
+  const uppercaseKey = key.toUpperCase();
+  const newKey = lowercaseKey + uppercaseKey;
+
+  const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const substitutionMap: Record<string, string> = {};
+
+  for (let i = 0; i < 52; i++) {
+    substitutionMap[newKey[i]] = alphabet[i];
+  }
+
+  const decodedString = input
+    .split('')
+    .map((char) => (substitutionMap[char] ? substitutionMap[char] : char))
+    .join('');
+
+  return decodedString;
 }
